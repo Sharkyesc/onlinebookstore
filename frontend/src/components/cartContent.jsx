bb4339b945707fe58b830a44dde1d90abb770b24
Sharkyesc <150518604+Sharkyesc@users.noreply.github.com>
2024-05-16 14:54:24 +0800

    cart页面组件化

--- frontend/src/components/cartContent.jsx
+++ frontend/src/components/cartContent.jsx
@@ -1,70 +1,96 @@
-/*
-
 import { React, useState, useEffect } from 'react';
 import { InputNumber, Checkbox } from 'antd';
 import { Input, Table, Space, Button, Modal } from 'antd';
 import { DeleteOutlined } from '@ant-design/icons';
+import { changeCartItemNumber, deleteCartItem } from '../service/cart';
 
 const { Search } = Input;
 const { confirm } = Modal;
 
 
-const columns = [
-    {
-      dataIndex: 'selected',
-      key: 'selected',
-      render: (selected, record) => (
-        <Checkbox checked={selected} onChange={(e) => handleCheckboxChange(record.id, e.target.checked)} />
-      ),
-    },
-    {
-      title: '封面',
-      dataIndex: 'coverSrc',
-      key: 'cover',
-      render: (coverSrc) => <img src={coverSrc} alt="Cover" style={{ width: 80 }} />,
-    },
-    {
-      title: '标题',
-      dataIndex: 'title',
-      key: 'title',
-    },
-    {
-      title: '单价',
-      dataIndex: 'price',
-      key: 'price',
-    },
-    {
-      title: '数量',
-      dataIndex: 'quantity',
-      key: 'quantity',
-      render: (text, record) => (
-        <InputNumber
-            min={1}
-            max={record.stocks}
-            defaultValue={text}
-            onChange={(value) => handleQuantityChange(record.id, value)}
-        />
-      ),
-    },
-    {
-      title: '金额',
-      dataIndex: 'total',
-      key: 'total',
-    },
-    {
-      title: '操作',
-      key: 'action',
-      render: (record) => (
-      <Space size="middle">
-        <Button icon={<DeleteOutlined /> } type="link" onClick={() => showDeleteConfirm(record.id)}  danger>
-          删除
-        </Button>
-      </Space>
-      ),
-    },
-  ];
+const CartContent = ({ cartData }) => {
+  const handleDelete = (id) => {
+    deleteCartItem(id)
+    .then(response => {
+      alert(response.message); 
+    })
+    .catch(error => console.error('There was a problem with deleting item from cart:', error));
+  };
+  
+  const handleQuantityChange = (id) => {
+    changeCartItemNumber(id, 1)
+    .then(response => {
+      alert(response.message); 
+    })
+    .catch(error => console.error('There was a problem with changing number of item:', error));
+  
+  };
+  
+  const showDeleteConfirm = (productId) => {
+    confirm({
+      title: '确定从购物车删除这本书吗?',
+      content: '删除后将无法恢复',
+      okText: '是',
+      okType: 'danger',
+      cancelText: '否',
+      onOk() {
+        console.log('OK');
+        handleDelete(productId);
+      },
+      onCancel() {
+        console.log('Cancel');
+      },
+    });
+  }
+  
+  const columns = [
+      {
+        title: '封面',
+        dataIndex: 'coverSrc',
+        key: 'cover',
+        render: (coverSrc) => <img src={coverSrc} alt="Cover" style={{ width: 80 }} />,
+      },
+      {
+        title: '标题',
+        dataIndex: 'title',
+        key: 'title',
+      },
+      {
+        title: '单价',
+        dataIndex: 'price',
+        key: 'price',
+      },
+      {
+        title: '数量',
+        dataIndex: 'quantity',
+        key: 'quantity',
+        render: (text, record) => (
+          <InputNumber
+              min={1}
+              max={record.stocks}
+              defaultValue={text}
+              onChange={(value) => handleQuantityChange(record.id, value)}
+          />
+        ),
+      },
+      {
+        title: '金额',
+        dataIndex: 'total',
+        key: 'total',
+      },
+      {
+        title: '操作',
+        key: 'action',
+        render: (record) => (
+        <Space size="middle">
+          <Button icon={<DeleteOutlined /> } type="link" onClick={() => showDeleteConfirm(record.id)}  danger>
+            删除
+          </Button>
+        </Space>
+        ),
+      },
+    ];
 
-const CartContent = () => {
     return (
         <div>
             <h1 style={{marginLeft: 10}}>购物车</h1>
@@ -78,5 +104,3 @@ const CartContent = () => {
 }
 
 export default CartContent;
-
-*/
--- frontend/src/pages/cart.jsx
+++ frontend/src/pages/cart.jsx
@@ -1,13 +1,10 @@
 import { React, useState, useEffect } from 'react';
 import NavBar from '../components/navBar'; 
-import { Layout, InputNumber, Checkbox } from 'antd';
-import { Input, Table, Space, Button, Modal } from 'antd';
-import { DeleteOutlined } from '@ant-design/icons';
+import { Layout } from 'antd';
 import { getCartItems } from '../service/cart';
+import CartContent from '../components/cartContent';
 
 const { Header, Content, Footer } = Layout;
-const { Search } = Input;
-const { confirm } = Modal;
 
 const CartPage = () => {
     const userInfo = {
@@ -25,104 +22,7 @@ const CartPage = () => {
     useEffect(() => {
         getCartData();
     }, []);
-  
-    const handleQuantityChange = (productId, quantity) => {
-      const updatedCartData = cartData.map(item => {
-          if (item.id === productId) {
-              item.quantity = quantity;
-              item.total = `￥${(parseFloat(item.price.replace('￥', '')) * quantity).toFixed(2)}`;
-            
-          }
-          return item;
-      });
-      setCartData(updatedCartData);
-    };
 
-    const handleCheckboxChange = (productId, checked) => {
-      const updatedCartData = cartData.map(item => {
-        if (item.id === productId) {
-          item.selected = checked;
-        }
-        return item;
-      });
-      setCartData(updatedCartData);
-    };
-    
-
-    const showDeleteConfirm = (productId) => {
-      confirm({
-        title: '确定从购物车删除这本书吗?',
-        content: '删除后将无法恢复',
-        okText: '是',
-        okType: 'danger',
-        cancelText: '否',
-        onOk() {
-          console.log('OK');
-        },
-        onCancel() {
-          console.log('Cancel');
-        },
-      });
-    }
-
-
-    const calculateTotalPrice = () => {
-      let totalPrice = 0;
-      cartData.forEach(item => {
-        if (item.selected) {
-          totalPrice += parseFloat(item.price.replace('￥', '')) * item.quantity;
-        }
-      });
-      return `￥${totalPrice.toFixed(2)}`;
-    };
-    
-    const columns = [
-    {
-      dataIndex: 'selected',
-      key: 'selected',
-      render: (selected, record) => (
-        <Checkbox checked={selected} onChange={(e) => handleCheckboxChange(record.id, e.target.checked)} />
-      ),
-    },
-    {
-      title: '封面',
-      dataIndex: 'coverSrc',
-      key: 'cover',
-      render: (coverSrc) => <img src={coverSrc} alt="Cover" style={{ width: 80 }} />,
-    },
-    {
-      title: '标题',
-      dataIndex: 'title',
-      key: 'title',
-    },
-    {
-      title: '单价',
-      dataIndex: 'price',
-      key: 'price',
-    },
-    {
-      title: '数量',
-      dataIndex: 'quantity',
-      key: 'quantity',
-      render: (record) => record.quantity
-    },
-    {
-      title: '金额',
-      dataIndex: 'total',
-      key: 'total',
-    },
-    {
-      title: '操作',
-      key: 'action',
-      render: (record) => (
-      <Space size="middle">
-        <Button icon={<DeleteOutlined /> } type="link" onClick={() => showDeleteConfirm(record.id)}  danger>
-          删除
-        </Button>
-      </Space>
-      ),
-    },
-  ];
   
     return ( cartData && 
         <Layout className="layout">
@@ -130,14 +30,9 @@ const CartPage = () => {
                 <NavBar username={userInfo.username} avatarSrc={userInfo.avatarSrc} />
             </Header>
             <Content className="shoppingcart-container" style={{width: '90%', marginLeft: '5%'}} >
-                <div>
-                    <h1 style={{marginLeft: 10}}>购物车</h1>
-                    <Search placeholder="输入书名搜索" style={{ width: 200, marginBottom: 16, marginLeft: 10 }} />
-                    <Table columns={columns} dataSource={cartData} pagination={{ pageSize: 5 }} />
-                    {/*<div style={{ position: 'fixed', bottom: 0, right: '5%', width:'100vh' }}>
-                        <h2>总计：{calculateTotalPrice()} 点击结算</h2>
-                    </div>*/}
-                </div>
+                <CartContent 
+                  cartData={ cartData }
+                />
             </Content>
             <Footer style={{ textAlign: 'center' }}>©2024</Footer>
         </Layout>
